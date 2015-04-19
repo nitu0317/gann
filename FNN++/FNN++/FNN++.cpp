c75184dce275192bfefdbcc223350ec4721b7d37
Phillip Kuznetsov <superpker@gmail.com>
2015-04-19 17:26:56 -0600

    Got polynomial to work, but there are complications regarding the possibility of using linear interpolation: there would have to be some extra coding in place to account for piece wise functions.

--- FNN++/FNN++/FNN++.cpp
+++ FNN++/FNN++/FNN++.cpp
@@ -9,8 +9,14 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
     auto f = [](double x) { return 2*x; };
-
-    
+	std::vector<double> poly1{ -1, 1 };
+	std::vector<double> poly2{ -1, 1 };
+	std::vector<double> ya = fnn::Math::PolyMult(poly1, poly2);
+	std::cout << ya[0];
+	for (int i = 1; i < ya.size(); i++)
+	{
+		std::cout << " + " << ya[i] << "x^" << i;
+	}
     system("pause");
 
 
--- FNN++/FNNLib/FNNMath.cpp
+++ FNN++/FNNLib/FNNMath.cpp
@@ -10,6 +10,7 @@
 #include <time.h>
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 ///=================================================================================================
 /// <summary>   Numerically integrates any integrable function using Simpson's rule with auto
@@ -95,11 +96,134 @@ double fnn::Math::GaussianReal(double mean, double dev)
     return distribution(engine);
 }
 
-double fnn::Math::LERP(std::vector<double> data)
+///=================================================================================================
+/// <summary>   A polynomial multiplication helper </summary>
+///
+/// <remarks>   Phillip Kuznetsov, 4/19/2015. </remarks>
+///
+/// <param name="poly1">A vector of the coefficients. Each index is the power which x is raised to</param>
+///
+/// <param name="poly2">A vector of the second polynomial coefficients./param>
+/// 
+/// <returns>   A vector of coefficients for hte polynomial </returns>
+///-------------------------------------------------------------------------------------------------
+
+std::vector<double> fnn::Math::PolyMult(std::vector<double> poly1, std::vector<double> poly2)
 {
-	auto func = [double y](double x){
+	std::vector<double> retPoly;
+	for (auto i = 0; i < poly1.size(); i++)
+	{
+		for (auto j = 0; j < poly2.size(); j++)
+		{
+			//The term  = : x^term of hte polynomial. Also the index of the output vector
+			int term = i*j;
+			double coeff = poly1[i] * poly2[i];
+			try
+			{
+				if (retPoly.size() == term)
+				{
+					retPoly.push_back(coeff);
+				}
+				else if (retPoly.size() < term)
+				{
+					throw 1;
+				}
+				else
+				{
+					retPoly[term] += coeff;
+				}
+			}
+			catch (int e)
+			{
+				std::cout << "Error " << e << "occurred in poylnomial multiplication";
+			}
+		}
+	}
+	return retPoly;
+}
+
+///=================================================================================================
+/// <summary>   A linear interpolation algorithm </summary>
+///
+/// <remarks>   Phillip Kuznetsov, 4/18/2015. </remarks>
+///
+/// <param name="data"> 2D vector of input data points </param>
+///
+/// <returns>   A linear interpolation function </returns>
+///-------------------------------------------------------------------------------------------------
+
+std::function<double(double)> fnn::Math::LERP(std::vector<std::vector<double>> data)
+{
+	auto func = [](double x){
 		return x;
 	};
+	return func;
+}
+
+///=================================================================================================
+/// <summary>   A polynomial interpolation algorithm according to http://en.wikipedia.org/wiki/Polynomial_interpolation </summary>
+///
+/// <remarks>   Phillip Kuznetsov, 4/19/2015. </remarks>
+///
+/// <param name="data"> 2D vector of input data points. Each row is a point. </param>
+///
+/// <returns>   A polynomial interpolation function </returns>
+///-------------------------------------------------------------------------------------------------
+
+std::function<double(double)> fnn::Math::PERP(std::vector<std::vector<double>> data)
+{
+	//coefficient vector such that each num is a coefficient to x^index.
+	std::vector<double> coef;
+	
+	for (auto i = 0; i < data.size(); i++)
+	{
+		std::vector<double> pti = data[i];
+		std::vector<double> tempCf;
+		double denom = 1;
+		for (auto j = 0; j < data.size(); j++)
+		{
+			if (i == j) continue;
+			std::vector<double> ptj = data[j];
+			std::vector<double> poly{ -ptj[0], 1 };
+			if (tempCf.size() == 0)
+			{
+				tempCf = poly;
+				continue;
+			}
+			tempCf = fnn::Math::PolyMult(tempCf, poly);
+			denom *= pti[0] - ptj[0];
+		}
+		double mult = denom * pti[1];
+		for (auto i = 0; i < tempCf.size(); i++)
+		{
+			try
+			{
+				if (coef.size() == i)
+				{
+					coef.push_back(tempCf[i] * mult);
+				}
+				else if (coef.size() < i)
+				{
+					throw 1;
+				}
+				else
+				{
+					coef[i] += tempCf[i] * mult;
+				}
+			}
+			catch (int e)
+			{
+				std::cout << "Mismatch of tempCF and coef";
+			}
+		}
+	}
+	auto func = [coef](double x){
+		double output = 0;
+		for (auto i = 0; i < coef.size(); i++)
+		{
+			output += coef[i] * pow(x, i);
+		}
+		return output; };
 	return func;
 }
 
