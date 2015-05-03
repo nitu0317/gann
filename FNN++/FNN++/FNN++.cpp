824dbcde5579408a651b5093c455ff16288ba72f
William Guss <carlostnb@gmail.com>
2015-05-03 15:41:53 -0600

    Removing test code which was migrated to FNNTest.sln, @philkuz

--- FNN++/FNN++/FNN++.cpp
+++ FNN++/FNN++/FNN++.cpp
@@ -13,28 +13,9 @@ using namespace fnn;
 using namespace std;
 
 
-void testMath();
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    /*vector<vector<double>> dataset;
-    vector<double> range{ -50, 50 };
-    for (auto i = 0; i < 50; i++)
-    {
-    vector<double> point;
-    for (auto j = 0; j < 2; j++)
-    {
-    auto fRand = [range](){
-    double f = (double)rand() / RAND_MAX;
-    return range[0] + f *(range[1] - range[0]);
-    };
-    point.push_back(fRand());
-    }
-    dataset.push_back(point);
-    }
-    auto poly = Math::PERP(dataset);
-    */
-
     //Construct a network
     Network net = Network();
     net.Activator = Sigmoid::Tanh();
@@ -58,14 +39,3 @@ int _tmain(int argc, _TCHAR* argv[])
 	return 0;
 }
 
-
-void testMath(){
-  
-    system("pause");
-    //A test of LERP
-    vector<vector<double>> tanhdata = { { -5, -0.9999092043 }, { -4, -0.9993292997 }, { -3.75, -0.998894 }, { -3.5, -0.998178 }, { -3.25, -0.996998 }, { -3., -0.995055 }, { -2.75, -0.99186 }, { -2.5, -0.986614 }, { -2.25, -0.978026 }, { -2., -0.964028 }, { -1.75, -0.941376 }, { -1.5, -0.905148 }, { -1.25, -0.848284 }, { -1., -0.761594 }, { -0.75, -0.635149 }, { -0.5, -0.462117 }, { -0.25, -0.244919 }, { 0., 0. }, { 0.25, 0.244919 }, { 0.5, 0.462117 }, { 0.75, 0.635149 }, { 1., 0.761594 }, { 1.25, 0.848284 }, { 1.5, 0.905148 }, { 1.75, 0.941376 }, { 2., 0.964028 }, { 2.25, 0.978026 }, { 2.5, 0.986614 }, { 2.75, 0.99186 }, { 3., 0.995055 }, { 3.25, 0.996998 }, { 3.5, 0.998178 }, { 3.75, 0.998894 } };
-    
-    system("pause");
-    function<double(double)> perpTest = Math::SSpline(tanhdata);
-    system("pause");
-}
