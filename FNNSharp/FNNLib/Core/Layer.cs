68cfd806ea16a1aa15dc234eb1e44495e8e263d4
William Guss <carlostnb@gmail.com>
2015-06-25 11:10:08 -0600

    Adding a suggested constructor to the Core Layer class

--- FNNSharp/FNNLib/Core/Layer.cs
+++ FNNSharp/FNNLib/Core/Layer.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace FNNLib.Core
 {
+    /// <summary>
+    /// Describes the general class of layers, \Sigma_l: A -> B.
+    /// Subclasses include n1,n2, and f, all of which are described in the white paper.
+    /// </summary>
+    /// <typeparam name="A">The set from which the layer maps.</typeparam>
+    /// <typeparam name="B">The set to which the layer maps.</typeparam>
     class Layer<A,B>
     {
+        public Layer(int Z_X, int Z_Y)
+        {
+            this.Z_X = Z_X;
+            this.Z_Y = Z_Y;
+        }
+
+
+
+        public int Z_X { get; set; }
+
+        public int Z_Y { get; set; }
     }
 }
