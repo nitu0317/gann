f7df4f0cd1d1018d3d7a50c4af9a14810efa555d
William Guss <carlostnb@gmail.com>
2015-07-10 16:54:38 -0600

    Implemted real closed intervals.

---
+++ FNNSharp/FNNLib/Util/Interval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNNLib.Util
+{
+    /// <summary>
+    /// Represents a real closed interval. 
+    /// </summary>
+    public class Interval
+    {
+        /// <summary>
+        /// Defines the interval with a left and right hand.
+        /// </summary>
+        /// <param name="a">The left hand.</param>
+        /// <param name="b">The right hand.</param>
+        public Interval(double a, double b)
+        {
+            this.A = a > b ? b : a;
+            this.B = a > b ? a : b;
+        }
+
+        public double A { get; set; }
+        public double B { get; set; }
+    }
+}
