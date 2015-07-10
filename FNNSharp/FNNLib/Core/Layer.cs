85c0da0b11eb869f58f1ff183eb7622915c864d3
William Guss <carlostnb@gmail.com>
2015-07-10 16:55:03 -0600

    Partially inmplemted layers with a generalized feed forward format.

--- FNNSharp/FNNLib/Core/Layer.cs
+++ FNNSharp/FNNLib/Core/Layer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FNNLib.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,22 +9,34 @@ namespace FNNLib.Core
 {
     /// <summary>
     /// Describes the general class of layers, \Sigma_l: A -> B.
-    /// Subclasses include n1,n2, and f, all of which are described in the white paper.
+    /// Subclasses include n,n1,n2, and f, all of which are described in the white paper.
     /// </summary>
     /// <typeparam name="A">The set from which the layer maps.</typeparam>
     /// <typeparam name="B">The set to which the layer maps.</typeparam>
     class Layer<A,B>
     {
-        public Layer(int Z_X, int Z_Y)
+
+        public Layer(int Z_X, int Z_Y, Interval R)
         {
             this.Z_X = Z_X;
             this.Z_Y = Z_Y;
+            this.R = R;
         }
 
+        /// <summary>
+        /// Feeds the algorithm forward, yielding an output.
+        /// We do not wish to contain anterior layers, and let the network handle propagation
+        /// in an interative fashion.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public abstract B FeedForward(A input);
 
 
         public int Z_X { get; set; }
 
         public int Z_Y { get; set; }
+
+        public Interval R { get; set; }
     }
 }
---
+++ FNNSharp/FNNLib/Core/Layers/FunctionalLayer.cs
@@ -0,0 +1,13 @@
+﻿using MathNet.Numerics.Interpolation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNNLib.Core.Layers
+{
+    class FunctionalLayer : Layer<IInterpolation, IInterpolation>
+    {
+    }
+}
