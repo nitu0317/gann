455905cae9cb3295584e9bf14b7fc3c51ce60dec
William Guss <carlostnb@gmail.com>
2015-07-10 19:11:26 -0600

    Adding a functional layer test

---
+++ FNNSharp/FNNTest/FunctionalLayerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FNNLib.Core.Layers;
+using FNNLib.Util;
+
+namespace FNNTest
+{
+    /// <summary>
+    /// Tests an F functional layer
+    /// </summary>
+    [TestClass]
+    public class FunctionalLayerTest
+    {
+        /// <summary>
+        /// Initializes the testing sweet.
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            f = new FunctionalLayer(10, 10, Interval.AbsoluteUnitBall);
+
+        }
+
+        /// <summary>
+        /// Tests the feedforward propagation algorithm,
+        /// </summary>
+        [TestMethod]
+        public void FeedForwardTest()
+        {
+            f.FeedForward(new FuncInterpolation((x) => x));
+        }
+
+        FunctionalLayer f;
+    }
+}
