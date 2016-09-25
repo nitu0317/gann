aef12817528afedb825d1151b48512cf9efcf337
MadcowD <carlostnb@gmail.com>
2016-09-25 14:01:12 -0700

    Working n_discrete layers.

--- gann/layers/n_discrete.py
+++ gann/layers/n_discrete.py
@@ -4,16 +4,16 @@ from layer import Layer
 from common.utils import random_tensor
 
 class NDiscrete(Layer):
-	def __init__(self, num_neurons, activation):
+	def __init__(self, num_neurons, activation=tf.nn.relu):
 		self.num_neurons = num_neurons
 		self.activation = activation
 		super().__init__("n_discrete")
 
 	def make_layer(self, prev_layer):
-		input_tens= prev_layer.output()
-		W = random_tensor(input_tens.shape[1:] + [num_neurons])
-		b = random_tensor([num_neurons])
-		self.output = activation(tf.matmul(input_tens, W) + b)
+		input_tens= prev_layer.get_output()
+		W = random_tensor(prev_layer.get_shape()[1:] + [self.num_neurons])
+		b = random_tensor([self.num_neurons])
+		self.output = self.activation(tf.matmul(input_tens, W) + b)
 	
 	def get_output(self):
 		return self.output
