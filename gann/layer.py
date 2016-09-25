4bb51d123cd701788c9d1789be2ef7ff55c16a06
MadcowD <carlostnb@gmail.com>
2016-09-25 13:37:01 -0700

    Adding discrete layers and basic gann forward functionality.

--- gann/layer.py
+++ gann/layer.py
@@ -4,7 +4,7 @@ from abc import abstractmethod
 import tensorflow as tf
 
 class Layer(metaclass=ABCMeta):
-	def __init__(name="GANNLayer"):
+	def __init__(self, name="GANNLayer"):
 		"""
 		Instantiates a layer object.
 		This is used for storing hyper parameters.
@@ -13,21 +13,24 @@ class Layer(metaclass=ABCMeta):
 		self.name = name
 
 	@abstractmethod
-	def make_layer(prev_layer):
+	def make_layer(self, prev_layer):
 		"""
-		Makes the layer and returns the output tensor.
+		Makes the layer given the previous layers output
 		Args:
 		    prev_layer - The previous layer tensor.
+		"""
+		return NotImplemented
+
+	@abstractmethod
+	def get_output(self):
+		"""
+		Returns the the layers output tensor.
 		"""
 		return NotImplemented
 
 	#####################################
 	# Private Hooks
 	####################################
-	def _add_to_network(prev_layer):
+	def _add_to_network(self, prev_layer):
 		with tf.variable_scope(self.name):
-			output = make_layer(prev_layer)
-		if not output:
-			raise RuntimeError("make_layer must return a layer tensor")
-
-		return output 
+			make_layer(prev_layer)
