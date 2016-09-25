fdf2f8410d120f5e8172d1e059fa22351cf41473
MadcowD <carlostnb@gmail.com>
2016-09-25 13:01:34 -0700

    Cleaning repo and adding tensorflow basis.

--- docs/doxygen/html/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['experiment',['Experiment',['../classfnn_1_1_experiment.html',1,'fnn']]]
-];
--- docs/doxygen/html/search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['math',['Math',['../classfnn_1_1_math.html',1,'fnn']]]
-];
