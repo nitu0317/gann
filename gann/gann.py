091821b912f20d97245ca8d16163b7d7391bc8d4
MadcowD <carlostnb@gmail.com>
2016-09-25 13:37:01 -0700

    Adding discrete layers and basic gann forward functionality.

--- gann/gann.py
+++ gann/gann.py
@@ -1,1 +1,70 @@
-gann.py
+import tensorflow as tf
+
+
+from layer import Layer
+from layers.input import Input
+
+class GANN:
+	def __init__(self, input_shape):
+		"""
+		Initializes a generalized artificial neural network.
+		"""
+		# TODO: Specify GPU usage if on GPU
+		self.sess = tf.InteractiveSession()
+
+		# Make the inputs
+		in_layer = Input(input_shape)
+		in_layer._add_to_network(None):
+		self._layers = [in_layer]
+
+
+	def forward(self, in_tens):
+		result = self.sess.run(self.get_output_tensor(),
+			{self.get_input_op: in_tens})
+		return result
+
+
+	########################################
+	#             Construction             #
+	########################################
+	def add_layer(self, layer : Layer):
+		"""
+		Adds a layer to the GANN and calls make_layer
+		"""
+		layer._add_to_network(self._layers[-1])
+		self._layers.append(layer)
+
+
+	def make_training_method(self, loss=None):
+		"""
+		Makes a training method for the GANN.
+		If the loss is unspecified, we use MSE.
+
+		Returns the desired input and the optimizer.
+		"""
+		out_tens = self.get_output_tensor()
+
+		# TODO: Add Batch Norm, L2 Regularization.
+		self.y_input = tf.placeholder(dtype="float",shape=out_tens.shape,name="desired")
+		self.cost = tf.reduce_mean(tf.square(self.y_input - out_tens)) 
+		self.optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(self.cost)
+
+		return self.y_input, self.optimizer
+
+
+
+	########################################
+	#             Getters/Setters          #
+	########################################
+	def get_output_tensor(self):
+		return self._layers[-1].get_output()
+
+	def get_input_op(self):
+		# Slightly jank.
+		return self.layers[0].get_output()
+
+	def get_optimizer_op(self):
+		return self.optimizer
+
+	def get_desired_output_op(self):
+		return self.y_input
--- gann/layers/n_discrete.py
+++ gann/layers/n_discrete.py
@@ -1,12 +1,19 @@
 import tensorflow as tf
 
 from layer import Layer
+from common.utils import random_tensor
 
 class NDiscrete(Layer):
-	def __init__(num_neurons, activation):
+	def __init__(self, num_neurons, activation):
 		self.num_neurons = num_neurons
 		self.activation = activation
 		super().__init__("n_discrete")
 
-	def make_layer(prev_layer):
-		W1 =
+	def make_layer(self, prev_layer):
+		input_tens= prev_layer.output()
+		W = random_tensor(input_tens.shape[1:] + [num_neurons])
+		b = random_tensor([num_neurons])
+		self.output = activation(tf.matmul(input_tens, W) + b)
+	
+	def get_output(self):
+		return self.output
